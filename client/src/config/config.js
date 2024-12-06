55f87fe8732ce03a75a31a52be1b7b784bc99821
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2024-12-06 22:19:55 +0530

    allowed all origin

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,8 +2,8 @@ export const YOUTUBE_BASE_URL = "https://www.youtube.com/watch?v=";
 
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
-// export const API_BASE_URL = "http://localhost:3000/api/v1";
-export const API_BASE_URL =
-  "https://entertainment-app-kkle.onrender.com/api/v1";
+export const API_BASE_URL = "http://localhost:3000/api/v1";
+// export const API_BASE_URL =
+//   "https://entertainment-app-kkle.onrender.com/api/v1";
 
 export const IMDB_BASE_URL = "https://www.imdb.com/title";
