0289f2b42f657126083480ccf0042b894f4801a4
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2024-12-07 11:15:38 +0530

    fixed bugs

--- client/src/custom_hooks/useLoadLoggedInUserData.jsx
+++ client/src/custom_hooks/useLoadLoggedInUserData.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { useGetLoggedInUserQuery } from "./features/mediaApi";
-import { setUserdata } from "./features/userSlice";
+import { useGetLoggedInUserQuery } from "../features/userSlice";
+import { setUserdata } from "../features/userSlice";
 
 export function useLoadLoggedInUserData() {
   const dispatch = useDispatch();
--- client/src/custom_hooks/useLoadUserBookmarks.jsx
+++ client/src/custom_hooks/useLoadUserBookmarks.jsx
@@ -1,14 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { toast, ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
-import "react-toastify/dist/ReactToastify.css";
-import { lazy, Suspense, useEffect } from "react";
-import Loader from "./components/general/Loader";
-import {
-  useGetLoggedInUserQuery,
-  useLazyGetUserBookmarksQuery,
-} from "./features/mediaApi";
-import { setUserdata } from "./features/userSlice";
+import { useEffect } from "react";
+import { useLazyGetUserBookmarksQuery } from "../features/mediaApi";
+
 import {
   setBookmarks,
   setBookmarkError,
