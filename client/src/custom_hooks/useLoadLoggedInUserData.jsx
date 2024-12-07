24624cca5704609d389c9eea988330d899a99f6b
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2024-12-07 11:22:41 +0530

    fixed bugs 3

--- client/src/custom_hooks/useLoadLoggedInUserData.jsx
+++ client/src/custom_hooks/useLoadLoggedInUserData.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { useGetLoggedInUserQuery } from "../features/userSlice";
+import { useGetLoggedInUserQuery } from "../features/mediaApi";
 import { setUserdata } from "../features/userSlice";
 
 export function useLoadLoggedInUserData() {
